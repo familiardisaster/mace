43f25d343e63d3690ca273e6d803df36624e42a2
similardisaster <116748806+familiardisaster@users.noreply.github.com>
2023-01-29 19:52:44 -0500

    Update xss.go

--- modules/xss.go
+++ modules/xss.go
@@ -1,7 +1,7 @@
 package modules
 
 import (
-	tools "Users/similar/Desktop/mace/pkg"
+	tools "/pkg"
 	"regexp"
 	"strings"
 )
@@ -21,7 +21,7 @@ func XSS(target string) {
 	if match {
 		regex := regexp.MustCompile(`\=(.)\&?`)
 		matches := regex.FindAllStringSubmatch(target, -1)
-		payloads, _ := tools.ReadLines(`/Users/similar/Desktop/mace/payloads/xss.txt`)
+		payloads, _ := tools.ReadLines(`/payloads/xss.txt`)
 		for _, match := range matches {
 			for _, payload := range payloads {
 				targets = append(targets, strings.ReplaceAll(target, match[1], payload))
